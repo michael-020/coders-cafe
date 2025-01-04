b3ae09bd6713e04b5f49700ebd849c76b562578e
Michael Hosamani <michaelhosamani26@gmail.com>
2025-01-04 21:57:42 +0530

    animated menu component

--- app/components/Menu.tsx
+++ app/components/Menu.tsx
@@ -1,13 +1,38 @@
-import React from 'react'
+"use client"
+import React, { useEffect } from 'react'
 import { MenuItem } from './MenuItem'
+import { motion, useAnimate, useInView } from 'framer-motion'
+
+
 
 const Menu = () => {
+  const [scope, animate] = useAnimate()
+  const isInView = useInView(scope)
+
+  useEffect(() => {
+    async function initAnimation() {
+      if(!isInView){
+        animate("#heading", {opacity: 0, y: 60} )
+        animate("#coffee-1", {opacity: 0, y: 60} )
+        animate("#coffee-2", {opacity: 0, y: 60} )
+        animate("#coffee-3", {opacity: 0, y: 60} )
+      }
+      else{
+        animate("#heading", {opacity: 1, y: 0}, {delay: 0.2, type: "spring"} )
+        animate("#coffee-1", {opacity: 1, y: 0}, {delay: 0.2, type: "spring"} )
+        animate("#coffee-2", {opacity: 1, y: 0}, {delay: 0.4, type: "spring"} )
+        animate("#coffee-3", {opacity: 1, y: 0}, {delay: 0.6, type: "spring"} )
+      }
+    }
+    initAnimation()
+  }, [isInView])
+
   return (
     <div className='h-[444px] w-screen px-44 py-12 '>
-      <div className=' h-full  flex flex-col items-center gap-5'>
+      <div ref={scope} className=' h-full  flex flex-col items-center gap-5'>
         {/* heading */}
         <div className=''>
-          <h4 className='text-center text-3xl font-bold'>
+          <h4 id='heading' className='text-center text-3xl font-bold'>
             <span className=''>Fresh and </span> <span className='text-amber-500'>Tasty coffee</span>
           </h4>
           <p className='text-center my-2'>
@@ -16,12 +41,17 @@ const Menu = () => {
         </div>
         {/* menu items */}
         <div className='grid grid-cols-3 gap-16 justify-items-stretch mt-8'>
-          <MenuItem imageURL='black-coffee.png' title='Black Coffee' description='lorem ipsum dolor sit amet, consectetur adipiscing elit.' />
-          <div className="relative -top-12 -z-10">
-
+          <motion.div id='coffee-1' 
+            
+          > 
+            <MenuItem  imageURL='black-coffee.png' title='Black Coffee' description='lorem ipsum dolor sit amet, consectetur adipiscing elit.' />
+          </motion.div>
+          <motion.div id='coffee-2' className="relative bottom-12">
             <MenuItem  imageURL='hot-coffee-2.png' title='Hot Coffee' description='lorem ipsum dolor sit amet, consectetur adipiscing elit.' />
-          </div>
-          <MenuItem imageURL='cold-coffee.png' title='Cold Coffee' description='lorem ipsum dolor sit amet, consectetur adipiscing elit.' />
+          </motion.div>
+          <motion.div id='coffee-3'>
+            <MenuItem imageURL='cold-coffee.png' title='Cold Coffee' description='lorem ipsum dolor sit amet, consectetur adipiscing elit.' />
+          </motion.div>
         </div>
       </div>
     </div>
--- app/components/MenuItem.tsx
+++ app/components/MenuItem.tsx
@@ -1,4 +1,6 @@
+"use client"
 import React from 'react'
+import { motion } from 'framer-motion'
 
 interface IMenuItem {
     imageURL: string,
@@ -6,10 +8,22 @@ interface IMenuItem {
     description: string
 }
 
+const imageVariant = {
+    hover: {
+      scale: 1.1,
+      y: -10,
+      boxShadow: "black",
+    }
+  }
+
 export const MenuItem = ({imageURL, title, description}: IMenuItem) => {
   return (
     <div className='h-60 w-72 flex flex-col items-center'>
-        <img src={imageURL} width={200} height={200} />
+        <motion.img src={imageURL} width={200} height={200} className='cursor-pointer [filter:drop-shadow(5px_15px_35px_#222)] '
+            // style={{"-webkit-filter": "drop-shadow(5px 15px 35px #222)", "filter": "drop-shadow(5px 5px 5px #222)"}}
+            variants={imageVariant}
+            whileHover={"hover"}
+        />
         <h3 className='text-amber-500 font-extrabold text-xl'>{title}</h3>
         <p className='text-center'>{description}</p>
     </div>
